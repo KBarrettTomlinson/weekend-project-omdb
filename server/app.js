43dbfaad17ecdf9ba7ad8808e142c9e9461bf211
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-04-08 10:36:31 -0500

    test server is listening

--- server/app.js
+++ server/app.js
@@ -3,10 +3,10 @@ var express = require( 'express' );
 var app = express();
 
 //db modules
-var davidBowie = require( 'modules/db.js');
+// var davidBowie = require( './modules/db.js');
 
 //route modules
-var index = require( 'routes/index.js' );
+// var index = require( './routes/index.js' );
 
 //app config
 app.set( 'port', (process.env.PORT || 3000 ));
@@ -20,7 +20,7 @@ app.use( bodyParser.urlencoded({extended: true}));
 app.use( express.static( 'server/public' ));
 
 //routes
-app.use( '/', index);
+// app.use( '/', index);
 
 //listens
 var port = app.get( 'port' );
