64a4a3ab376add242a46551f64b893f09bec2a7b
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-04-08 10:16:38 -0500

    completes initial file and directory setup

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: 'client/scripts/*.js',
+        dest: 'server/public/scripts/client.min.js'
+      }
+    },
+    copy: {
+      html: {
+        expand: true,
+        cwd: 'client/views',
+        src: ['index.html'],
+        dest: 'server/public/views/'
+      },
+      client: {
+        expand: true,
+        cwd: 'client/scripts',
+        src: ['client.js'],
+        dest: 'server/public/scripts/'
+      },
+      css: {
+        expand: true,
+        cwd: 'client/styles',
+        src: ['style.css'],
+        dest: 'server/public/styles/'
+      },
+      bootstrap: {
+        expand: true,
+        cwd: 'node_modules/bootstrap/dist/',
+        src: ['css/bootstrap.css',
+              'js/bootstrap.js',
+              'fonts/*.*'],
+        dest: 'server/public/vendors/bootstrap/'
+      },
+      angular: {
+        expand: true,
+        cwd: 'node_modules/angular/',
+        src: ['angular.js',
+              'angular.min.js',
+              'angular.min.js.map'],
+        dest: 'server/public/vendors/angular/'
+      }
+    },
+    watch: {
+      files: [
+        'client/**/*.*'
+      ],
+      tasks: ['uglify', 'copy']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['uglify', 'copy', 'watch']);
+};
