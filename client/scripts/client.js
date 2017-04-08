8590a57e814f22345d974ffb23f6289861738136
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-04-08 16:18:28 -0500

    gets search results from OMDB, displays the object to the DOM

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -7,12 +7,31 @@ klmApp.controller( 'MainController' , [ '$scope', '$http', function( $scope, $ht
   $scope.searchObject = {};
 
   $scope.findMovie = findMovie;
+
   function findMovie(object){
     var copy = angular.copy(object);
     var title = copy.title;
 
     object.title = '';
 
-  }
+    $http.get('http://www.omdbapi.com/?t=' + title + '&y=&plot=full&r=json').
+      then(function(response){
+        console.log(response);
+        var movie = {};
+        console.log(response.data.Title);
+        movie.title = response.data.Title;
+        movie.director = response.data.Director;
+        movie.poster = response.data.Poster;
+        movie.year = response.data.Year;
+        movie.writer = response.data.Writer;
+        console.log(movie);
+        searchResult.movie = movie;
+      });
+      
+    var searchResult = {};
+    $scope.searchResult = searchResult;
+    console.log("search result object", $scope.searchResult);
+
+  }//ends findMovie
 
 }]);
