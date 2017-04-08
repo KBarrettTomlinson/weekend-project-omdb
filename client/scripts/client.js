e33bd26633b6a40f92e36b4277c3e3c1b5e15643
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-04-08 15:42:39 -0500

    adds click functionality

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -1,2 +1,18 @@
 // setup angular module
 var klmApp = angular.module( 'klmApp', []);
+
+
+// setup main controller
+klmApp.controller( 'MainController' , [ '$scope', '$http', function( $scope, $http){
+  $scope.searchObject = {};
+
+  $scope.findMovie = findMovie;
+  function findMovie(object){
+    var copy = angular.copy(object);
+    var title = copy.title;
+
+    object.title = '';
+
+  }
+
+}]);
