45becc5b37e0509da7abf4c47c5f3d3c198fbc40
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-04-08 10:31:46 -0500

    spins up server

---
+++ server/app.js
@@ -0,0 +1,32 @@
+//base modules
+var express = require( 'express' );
+var app = express();
+
+//db modules
+var davidBowie = require( 'modules/db.js');
+
+//route modules
+var index = require( 'routes/index.js' );
+
+//app config
+app.set( 'port', (process.env.PORT || 3000 ));
+
+//middleware
+var bodyParser = require( 'body-parser' );
+
+app.use( bodyParser.json());
+app.use( bodyParser.urlencoded({extended: true}));
+
+app.use( express.static( 'server/public' ));
+
+//routes
+app.use( '/', index);
+
+//listens
+var port = app.get( 'port' );
+app.listen( port, function(){
+  console.log( "I'm listening for you at port:", port, " I won't drop the ball!");
+});
+
+//exports
+module.export = app;
