c43b647c6a0f88c8b8109203bcc12354cf268d6d
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-06-13 12:53:53 -0500

    moves functions out of MovieService Return

--- client/scripts/factories/MovieService.js
+++ client/scripts/factories/MovieService.js
@@ -6,61 +6,67 @@ klmApp.factory( 'MovieService', ['$http', function($http){
   var classesObject = {};
   classesObject.classes = listOfClasses;
 
-  $http.get('/favorites').then(function(response){
-    console.log("get all favorites",response);
-    favorites.list = response.data;
-  });
+  function getFavorites(){
+    $http.get('/favorites').then(function(response){
+      console.log("get all favorites",response);
+      favorites.list = response.data;
+    });
+  }//ends getFavorites
+  getFavorites();
 
+  function findMovie(object){
+    var copy = angular.copy(object);
+    var title = copy.title;
+    if ("undefined" === typeof object._id){
+      object.title = '';
+    }
+    $http.get('http://www.omdbapi.com/?t=' + title + '&y=&plot=full&r=json').
+      then(function(response){
+        var movie = {};
+        movie.title = response.data.Title;
+        movie.director = response.data.Director;
+        movie.poster = response.data.Poster;
+        movie.year = response.data.Year;
+        movie.writer = response.data.Writer;
+        movie.plot = response.data.Plot;
+        searchResult.movie = movie;
+        listOfClasses.pop("hidden");
+      });//ends response
+  }//ends findMovie
+
+  function addToFavorites(title,year){
+    var favoriteObject = {};
+    var newTitle = angular.copy(title);
+    var newYear = angular.copy(year);
+    favoriteObject.title = newTitle;
+    favoriteObject.year = newYear;
+
+    $http.post('/favorites/addFavorite', favoriteObject).then(function(response){
+      $http.get('/favorites').then(function(response){
+        console.log(response);
+        favorites.list = response.data;
+      });// ends get to favorites
+    });//ends post to addFavorite
+  }//ends addToFavorites
+
+  function deleteMovie(id){
+    var deleteId = {};
+    deleteId.id = id;
+    $http.delete('/favorites/deleteFavorite/'+ id).then(function(response){
+      console.log(response);
+      $http.get('/favorites').then(function(response){
+        favorites.list = response.data;
+      });//ends get to favorites
+    });//ends delete to deleteFavorite
+  }//ends deleteMovie
 
   return{
     classesObject: classesObject,
     searchResult: searchResult,
     favorites: favorites,
-    findMovie: function (object){
-      var copy = angular.copy(object);
-      var title = copy.title;
-      if ("undefined" === typeof object._id){
-        object.title = '';
-      }
-      $http.get('http://www.omdbapi.com/?t=' + title + '&y=&plot=full&r=json').
-        then(function(response){
-          var movie = {};
-          movie.title = response.data.Title;
-          movie.director = response.data.Director;
-          movie.poster = response.data.Poster;
-          movie.year = response.data.Year;
-          movie.writer = response.data.Writer;
-          movie.plot = response.data.Plot;
-          searchResult.movie = movie;
-          listOfClasses.pop("hidden");
-        });//ends response
-    },//ends findMovie
-
-    addToFavorites:   function (title, year){
-        var favoriteObject = {};
-        var newTitle = angular.copy(title);
-        var newYear = angular.copy(year);
-        favoriteObject.title = newTitle;
-        favoriteObject.year = newYear;
-
-        $http.post('/favorites/addFavorite', favoriteObject).then(function(response){
-          $http.get('/favorites').then(function(response){
-            console.log(response);
-            favorites.list = response.data;
-          });// ends get to favorites
-        });//ends post to addFavorite
-      },//ends addToFavorites
-
-    deleteMovie: function (id){
-        var deleteId = {};
-        deleteId.id = id;
-        $http.delete('/favorites/deleteFavorite/'+ id).then(function(response){
-          console.log(response);
-          $http.get('/favorites').then(function(response){
-            favorites.list = response.data;
-          });//ends get to favorites
-        });//ends delete to deleteFavorite
-      },//ends deleteMovie
+    findMovie: findMovie,
+    addToFavorites: addToFavorites,
+    deleteMovie: deleteMovie
   };//ends return
 
 }]);//ends factory
